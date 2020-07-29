11a40b569b73d55818e5af85a1e096ea11d120e6
net_pond <suparongn@gmail.com>
2020-07-29 16:25:49 +0700

    ref

--- api/facebook.js
+++ api/facebook.js
@@ -1,4 +1,3 @@
-const got = require('got')
 const rq = require('request-promise')
 const QUERY = "about,picture{url},fan_count,name"
 const ACCESS_TOKEN = 'EAAG4BSmPZAe0BAJY7m7gJMHo4PEuI7ZALkbwcahHtru424qdIC5Ft6yMtkWWa38QDy5tEEWbOeMRTcqK7Q5lLBNtI8teRDIB9SEqqEHAC6LObgINf7SEKZCmhxCiQ3pO0ScJzSfVkvbtoZAPP1W4TckbMfTXn3qZAJuA8lByb5AZDZD'
