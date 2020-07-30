3f1602c6446016f9f3af52b2f6a41fa544ba5148
net_pond <suparongn@gmail.com>
2020-07-30 11:31:11 +0700

    log

--- api/facebook.js
+++ api/facebook.js
@@ -20,7 +20,8 @@ async function facebook(message) {
             console.log("+++++>", name, "zone : ", zone)
         } else {
             let urlParams = new URLSearchParams(message)
-            console.log("22222222222", urlParams)
+            console.log("22222222222", searchParams.keys()[0])
+
         }
 
         // let pages = await searchPages(name)
