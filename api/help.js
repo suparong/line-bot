90e8842caf94b8524f39283373f0eec029ae2081
net_pond <suparongn@gmail.com>
2020-07-30 17:01:52 +0700

    help 2

--- api/help.js
+++ api/help.js
@@ -14,39 +14,75 @@ async function help() {
 async function formatHelp() {
     return {
         "type": "bubble",
-        "body":
-        {
+        "styles": {
+            "footer": {
+                "backgroundColor": "#42b3f4"
+            }
+        },
+        "header": {
             "type": "box",
-            "layout": "horizontal",
+            "layout": "vertical",
             "contents": [
                 {
                     "type": "text",
-                    "text": "วิธี add page Facebook",
-                    "size": "md"
+                    "text": "วิธี add page Facebook"
+                }
+            ]
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "margin": "md",
+                    "size": "sm",
+                    "color": "#666666",
+                    "text": `1.ใส่ link page (ex.https://www.facebook.com/TidPromo)`,
+                    "wrap": true
                 },
                 {
                     "type": "text",
-                    "text": "1.ใส่ link page (ex.https://www.facebook.com/TidPromo)",
-                    "size": "md"
+                    "margin": "md",
+                    "size": "sm",
+                    "color": "#666666",
+                    "text": `2.ใส่ zone มาด้วย ถ้าไม่ใส่มาจะเป็น none`,
+                    "wrap": true
                 },
                 {
                     "type": "text",
-                    "text": "ใส่ zone มาด้วย ถ้าไม่ใส่มาจะเป็น none",
-                    "size": "md"
+                    "margin": "md",
+                    "size": "sm",
+                    "color": "#666666",
+                    "text": `ตัวอย่าง`,
+                    "wrap": true
                 },
                 {
                     "type": "text",
-                    "text": "Ex.https://www.facebook.com/Mommy-Is-Here-108444714131126&zone=th",
-                    "size": "md"
-                },
+                    "margin": "md",
+                    "size": "sm",
+                    "color": "#666666",
+                    "text": `https://www.facebook.com/Mommy-Is-Here-108444714131126&zone=th
+                    Mommy-Is-Here-108444714131126&zone=th`,
+                    "wrap": true
+                }
+            ]
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
                 {
                     "type": "text",
-                    "text": "****** add แล้วไม่ได้เข้าเลยนะ ต้องรอ approve ก่อน",
-                    "size": "lg"
+                    "margin": "md",
+                    "size": "sm",
+                    "color": "#666666",
+                    "text": `****** add แล้วไม่ได้เข้าเลยนะ ต้องรอ approve ก่อน`,
+                    "wrap": true
                 }
             ]
         }
-
     }
 
 }
