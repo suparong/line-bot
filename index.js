c6c00698973d70b4cf701020d73bd765829e03ec
net_pond <suparongn@gmail.com>
2020-07-31 15:58:46 +0700

    add post status v6

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/statusApprove', async (req, res) => {
     let readyToSend = await formatData(req.body)
     console.log("======", readyToSend)
     pushBody(JSON.stringify(readyToSend))
-    // res.status(200).send(readyToSend)
+    res.status(200).send(true)
 })
 
 app.listen(port, async () => {
