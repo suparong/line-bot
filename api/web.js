656878e8ea330d048d79bc4362b26b3d515a247c
net_pond <suparongn@gmail.com>
2020-08-04 11:55:13 +0700

    log

--- api/web.js
+++ api/web.js
@@ -4,7 +4,7 @@ async function web(message) {
     console.log("============>", message)
     let webArray = message.split("=")
     const domain = webArray[1]
-    if (_.includes("http") || _.includes("https")) {
+    if (_.includes(domain, "http") || _.includes(domain, "https")) {
         let url_domain = new URL(domain)
         console.log("============> 1", url_domain.host)
         console.log("============> 1", url_domain.hostname)
