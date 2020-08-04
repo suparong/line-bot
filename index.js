2c10326965e7b4cc960caf57c6dc0c09eb3547dc
net_pond <suparongn@gmail.com>
2020-08-04 14:35:13 +0700

    test

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-// // Reply with two static messages
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-// // require('dotenv').config()
-// const port = process.env.PORT_API || 4000
-
-// const { reply, formatData } = require('./api/sendToLine')
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-
-// app.post('/webhook', (req, res) => {
-//     // console.log('webhook')
-//     reply(req)
-// })
-
-// app.post('/statusApprove', async (req, res) => {
-//     let readyToSend = await formatData(req.body)
-//     // console.log("======", JSON.stringify(readyToSend))
-//     pushBody(JSON.stringify(readyToSend))
-//     res.status(200).send(true)
-// })
-
-// app.listen(port, async () => {
-//     console.log('Starting  version 1.1');
-//     console.log('Starting node.js on port ' + `${port}`);
-// });
-
+// Reply with two static messages
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+// require('dotenv').config()
+const port = process.env.PORT_API || 4000
+
+const { reply, formatData } = require('./api/sendToLine')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+    // console.log('webhook')
+    reply(req)
+})
+
+app.post('/statusApprove', async (req, res) => {
+    let readyToSend = await formatData(req.body)
+    // console.log("======", JSON.stringify(readyToSend))
+    pushBody(JSON.stringify(readyToSend))
+    res.status(200).send(true)
+})
+
+app.listen(port, async () => {
+    console.log('Starting  version 1.1');
+    console.log('Starting node.js on port ' + `${port}`);
+});
