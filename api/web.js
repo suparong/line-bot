619a8799e21abb298726915f8dd87f8873eadb7b
net_pond <suparongn@gmail.com>
2020-08-04 14:53:43 +0700

    test

--- api/web.js
+++ api/web.js
@@ -7,12 +7,18 @@ async function web(message) {
     const domain = webArray[1]
     if (_.includes(domain, "http") || _.includes(domain, "https")) {
         let url_domain = new URL(domain)
-        let newdomain = (url_domain.host).split("www.")
-        console.log("============> 1", newdomain)
+        if (_.includes(url_domain, "www")) {
+            let newdomain = (url_domain.host).split("www.")
+            domain = newdomain[1]
+            console.log("============> 1", domain)
+        } else {
+            domain = newdomain[0]
+            console.log("============> 2", domain)
+        }
     } else {
-        console.log("============> 2", domain)
+        console.log("============> 3", domain)
     }
-
+    console.log("============> 4", domain)
 
 
 }
