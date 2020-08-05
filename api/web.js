2971622ace2f9cb9bbe8810f3f12d64a642adcb2
net_pond <suparongn@gmail.com>
2020-08-05 18:00:56 +0700

    ser ui  width box v3

--- api/web.js
+++ api/web.js
@@ -45,7 +45,7 @@ async function web(message) {
                 "type": "box",
                 "layout": "vertical",
                 "spacing": "md",
-                "offsetTop": "-30px"
+                "offsetTop": "-40px"
             }
         }
     }
