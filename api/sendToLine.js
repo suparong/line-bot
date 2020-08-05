a4e935b9a5a656a699f0cb11f611541f77322a17
net_pond <suparongn@gmail.com>
2020-08-05 11:11:39 +0700

    log

--- api/sendToLine.js
+++ api/sendToLine.js
@@ -15,7 +15,7 @@ const { web } = require('./web')
 async function reply(req) {
 
     let newres = await setBody(req)
-    // console.log("=============>", newres)
+    console.log("=============>", newres)
     /**
      * url :reply,push,multicast,Broadcast
      */
