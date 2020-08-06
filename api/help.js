8d44ae902545c8da4793eba3e6719cf6db5fe88f
net_pond <suparongn@gmail.com>
2020-08-06 18:13:59 +0700

    fix help

--- api/help.js
+++ api/help.js
@@ -12,82 +12,81 @@ async function help() {
 }
 
 async function formatHelpFB() {
-    return
-    {
+    return {
         "type": "flex",
-            "altText": "new messages",
-                "contents": {
+        "altText": "new messages",
+        "contents": {
             "type": "bubble",
-                "styles": {
+            "styles": {
                 "footer": {
                     "backgroundColor": "#42b3f4"
                 }
             },
             "header": {
                 "type": "box",
-                    "layout": "vertical",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "How To Add Facebook Page"
-                            }
-                        ]
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "How To Add Facebook Page"
+                    }
+                ]
             },
             "body": {
                 "type": "box",
-                    "layout": "vertical",
-                        "spacing": "md",
-                            "offsetTop": "-25px",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "margin": "md",
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "text": "   1. Send Facebook page’s link with zone",
-                                        "wrap": true
-                                    },
-                                    {
-                                        "type": "text",
-                                        "margin": "md",
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "text": "   Ex.https://www.facebook.com/TidPromo/&zone=th",
-                                        "wrap": true
-                                    },
-                                    {
-                                        "type": "text",
-                                        "margin": "md",
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "text": "    Zone List in Zanroo system:",
-                                        "wrap": true
-                                    },
-                                    {
-                                        "type": "text",
-                                        "margin": "md",
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "text": "   th, jp, ph, mm, my, au, kr, sg, hk, id, tw, kh, vn, us, in, nz, nl, fr, de, ru, ae, eg, lk, br, mx, la, pk, gb, bn, ca",
-                                        "wrap": true
-                                    },
-                                    {
-                                        "type": "text",
-                                        "margin": "md",
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "text": "   2. If your page does not exist in our system, page info will be shown.",
-                                        "wrap": true
-                                    },
-                                    {
-                                        "type": "text",
-                                        "margin": "md",
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "text": "   3. Submit your page and wait for an approval.",
-                                        "wrap": true
-                                    }
-                                ]
+                "layout": "vertical",
+                "spacing": "md",
+                "offsetTop": "-25px",
+                "contents": [
+                    {
+                        "type": "text",
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#666666",
+                        "text": "   1. Send Facebook page’s link with zone",
+                        "wrap": true
+                    },
+                    {
+                        "type": "text",
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#666666",
+                        "text": "   Ex.https://www.facebook.com/TidPromo/&zone=th",
+                        "wrap": true
+                    },
+                    {
+                        "type": "text",
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#666666",
+                        "text": "    Zone List in Zanroo system:",
+                        "wrap": true
+                    },
+                    {
+                        "type": "text",
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#666666",
+                        "text": "   th, jp, ph, mm, my, au, kr, sg, hk, id, tw, kh, vn, us, in, nz, nl, fr, de, ru, ae, eg, lk, br, mx, la, pk, gb, bn, ca",
+                        "wrap": true
+                    },
+                    {
+                        "type": "text",
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#666666",
+                        "text": "   2. If your page does not exist in our system, page info will be shown.",
+                        "wrap": true
+                    },
+                    {
+                        "type": "text",
+                        "margin": "md",
+                        "size": "sm",
+                        "color": "#666666",
+                        "text": "   3. Submit your page and wait for an approval.",
+                        "wrap": true
+                    }
+                ]
             }
         }
     }
