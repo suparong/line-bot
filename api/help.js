e663850e668807233da5cff2de5320f253234279
net_pond <suparongn@gmail.com>
2020-08-06 18:11:26 +0700

    fix help

--- api/help.js
+++ api/help.js
@@ -1,89 +1,97 @@
 const { assign } = require("lodash")
 
 async function help() {
-    let info = {
-        "type": "flex",
-        "altText": "new messages"
-    }
+    // let info = {
+    //     "type": "flex",
+    //     "altText": "new messages"
+    // }
 
-    let sendHelp = await formatHelp()
-    info.contents = sendHelp
-    return info
+    let sendHelp = await formatHelpFB()
+    // info.contents = sendHelp
+    return sendHelp
 }
 
-async function formatHelp() {
-    return {
-        "type": "bubble",
-        "styles": {
-            "footer": {
-                "backgroundColor": "#42b3f4"
+async function formatHelpFB() {
+    return
+    {
+        "type": "flex",
+            "altText": "new messages",
+                "contents": {
+            "type": "bubble",
+                "styles": {
+                "footer": {
+                    "backgroundColor": "#42b3f4"
+                }
+            },
+            "header": {
+                "type": "box",
+                    "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "How To Add Facebook Page"
+                            }
+                        ]
+            },
+            "body": {
+                "type": "box",
+                    "layout": "vertical",
+                        "spacing": "md",
+                            "offsetTop": "-25px",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "text": "   1. Send Facebook page’s link with zone",
+                                        "wrap": true
+                                    },
+                                    {
+                                        "type": "text",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "text": "   Ex.https://www.facebook.com/TidPromo/&zone=th",
+                                        "wrap": true
+                                    },
+                                    {
+                                        "type": "text",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "text": "    Zone List in Zanroo system:",
+                                        "wrap": true
+                                    },
+                                    {
+                                        "type": "text",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "text": "   th, jp, ph, mm, my, au, kr, sg, hk, id, tw, kh, vn, us, in, nz, nl, fr, de, ru, ae, eg, lk, br, mx, la, pk, gb, bn, ca",
+                                        "wrap": true
+                                    },
+                                    {
+                                        "type": "text",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "text": "   2. If your page does not exist in our system, page info will be shown.",
+                                        "wrap": true
+                                    },
+                                    {
+                                        "type": "text",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "text": "   3. Submit your page and wait for an approval.",
+                                        "wrap": true
+                                    }
+                                ]
             }
-        },
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "How To Add Facebook Page"
-                }
-            ]
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "md",
-            "offsetTop": "-25px",
-            "contents": [
-                {
-                    "type": "text",
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#666666",
-                    "text": `   1. Send Facebook page’s link with zone`,
-                    "wrap": true
-                }, {
-                    "type": "text",
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#666666",
-                    "text": `   Ex.https://www.facebook.com/TidPromo/&zone=th`,
-                    "wrap": true
-                },
-                {
-                    "type": "text",
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#666666",
-                    "text": `    Zone List in Zanroo system:`,
-                    "wrap": true
-                }, {
-                    "type": "text",
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#666666",
-                    "text": `   th, jp, ph, mm, my, au, kr, sg, hk, id, tw, kh, vn, us, in, nz, nl, fr, de, ru, ae, eg, lk, br, mx, la, pk, gb, bn, ca`,
-                    "wrap": true
-                },
-                {
-                    "type": "text",
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#666666",
-                    "text": `   2. If your page does not exist in our system, page info will be shown.`,
-                    "wrap": true
-                },
-                {
-                    "type": "text",
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#666666",
-                    "text": `   3. Submit your page and wait for an approval.`,
-                    "wrap": true
-                }
-            ]
         }
     }
+
 
 }
 
