b0cea13bdf9e35c9420f0fd13d82bcf7354c189a
net_pond <suparongn@gmail.com>
2020-08-06 15:22:25 +0700

    fix bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT_API || 4000
 
-const { reply, formatData } = require('./api/sendToLine')
+const { reply, formatData, pushBody } = require('./api/sendToLine')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
